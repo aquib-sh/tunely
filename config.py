9c9acbaa59e8aefb3bc67efc48a1227affddb9e0
aquib <shaikhaquib394@gmail.com>
2021-09-01 21:58:07 +0530

    Add google before token and secret_file variable.

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ data_dir = os.path.join(os.getcwd(), "internal")
 cache_file = "cache.json"
 cache_path = os.path.join(data_dir, cache_file)
 # ======================= GOOGLE APIs =============================
-google_token_file  = 'google_token.json'
-client_secret_file = 'google_client_secret.json'
+google_token_file         = 'google_token.json'
+google_client_secret_file = 'google_client_secret.json'
 
-client_secret_path    = os.path.join(data_dir, client_secret_file)
-google_api_token_path = os.path.join(data_dir, google_token_file)
+google_client_secret_path = os.path.join(data_dir, google_client_secret_file)
+google_api_token_path     = os.path.join(data_dir, google_token_file)
 
 google_api_scope = ["https://www.googleapis.com/auth/youtube"]
 
