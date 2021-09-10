efaa18385cac175482407ee1ec1274d826776964
aquib <shaikhaquib394@gmail.com>
2021-09-10 17:53:29 +0530

    Add fetch_playlists(), fetch_playlists_title() methods.

--- services/google/youtube.py
+++ services/google/youtube.py
@@ -27,7 +27,68 @@ class YouTubeCtl(GoogleServices):
     def __init__(self, token, scope: list):
         super().__init__(token, scope)
 
+
     def start_service(self, version: str):
-        """Builds YouTube Data Service."""
+        """Builds YouTube Data Service.
+        
+        Parameters
+        ----------
+        version: str
+            version of the YouTube Data API (example: v3)
+        """
         super().start_service("youtube", version)
 
+
+    def fetch_playlists(self, channel_id: str, max_results: int) -> dict:
+        """Returns the JSON data on the list of playlists
+           for a channel. 
+
+        Parameters
+        ----------
+        channel_id: str
+            id of channel where you want to get playlists from.
+            this channel id can be found in the channel link
+            example: https://www.youtube.com/channel/UC_aEa8K-EOJ3D6gOs7HcyNg
+            here UC_aEa8K-EOJ3D6gOs7HcyNg is the channel_id
+
+        max_results: int
+            max results to return as JSON 
+            ( if set to 2 then 2 playlists data will return)
+            (acceptable values are between 0 to 50)
+        """
+        request = self.service.playlists().list(
+            part="id, snippet,contentDetails", 
+            channelId=channel_id, 
+            maxResults=max_results
+        )
+        response = request.execute()
+
+        return response
+
+    
+    def fetch_playlists_title(self, channel_id: str, max_results: int) -> list:
+        """Returns the list containing titles of playlists 
+           for a particular YouTube channel.
+
+        Parameters
+        ----------
+        channel_id: str
+            id of channel where you want to get playlists from.
+            this channel id can be found in the channel link
+            example: https://www.youtube.com/channel/UC_aEa8K-EOJ3D6gOs7HcyNg
+            here UC_aEa8K-EOJ3D6gOs7HcyNg is the channel_id
+
+        max_results: int
+            max results to return as JSON 
+            ( if set to 2 then 2 playlists data will return)
+            (acceptable values are between 0 to 50)
+        """
+        response = self.fetch_playlists(channel_id, max_results)
+        titles = []
+        total_items = len(response['items'])
+
+        for i in range(0, total_items):
+            title = response['items'][i]['snippet']['title']
+            titles.append(title)
+
+        return titles
