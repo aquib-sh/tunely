15f1f58e2845b37b459b538197e29febd7dfea4f
aquib <shaikhaquib394@gmail.com>
2021-09-10 17:04:47 +0530

    Rename YouTubeHander to YouTubeCtl

--- services/google/youtube.py
+++ services/google/youtube.py
@@ -3,8 +3,10 @@
 
 from services.google.google_services import GoogleServices
 
-class YouTubeHandler(GoogleServices):
-    """Handles the functions of YouTube using YouTube Data API.
+class YouTubeCtl(GoogleServices):
+    """ YouTube Controller Class
+    
+    Controls the functions of YouTube using YouTube Data API.
     
     Parameters
     ----------
@@ -13,6 +15,14 @@ class YouTubeHandler(GoogleServices):
 
     scope: list
         Scope of the current token.
+
+    Methods
+    -------
+    start_service(version: str)
+        builds the service by calling start_service from
+        super class and passing the parameters for YouTube service.
+        returns a service object.
+
     """
     def __init__(self, token, scope: list):
         super().__init__(token, scope)
