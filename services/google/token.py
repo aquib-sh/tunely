d769bd3d0701b4a5b727aeb211296ea0c84ab55e
aquib-sh <shaikhaquib394@gmail.com>
2022-03-30 20:32:25 +0530

    Add TokenRetriever

--- services/google/token.py
+++ services/google/token.py
@@ -1,16 +1,17 @@
-import os
 import json
-from googleapiclient.discovery import build
+import os
+
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
- 
+
 class TokenGenerator:
-    
     def generate_google_api_token(self, client_secret: str, scopes: list):
         """Generates a Google API Token from scratch, user login and auth would be required.
-        
+
         Parameters
         ----------
         client_secret: str
@@ -24,10 +25,9 @@ class TokenGenerator:
         creds = flow.run_console()
         return creds
 
-
     def export_token(self, token, output_path: str):
         """Outputs the token as JSON file.
-        
+
         Parameters
         ----------
         token
@@ -40,25 +40,27 @@ class TokenGenerator:
             fp.write(token.to_json())
 
 
+class TokenRetriever:
     def retrieve_google_api_token(self, token_path: str, scopes: list):
         """Retrieves the token from a file.
-        
+
         Parameters
         ----------
         token_path: str
             Path to the google_api_token json file.
-        
+
         scopes: list
             Scopes of the api token.
         """
         if os.path.exists(token_path):
             creds = Credentials.from_authorized_user_file(token_path, scopes)
-        else: return None
+        else:
+            return None
 
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
-            else : return None
+            else:
+                return None
         return creds
-    
