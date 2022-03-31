45a98e0335a2109870697804c7d0eb4697d70e48
aquib-sh <shaikhaquib394@gmail.com>
2022-03-31 21:23:43 +0530

    Integrate DBManager
    1. Add new_link_posted() method
    2. Rename search_in_playlists() -> playlist_exists()

--- services/service_manager.py
+++ services/service_manager.py
@@ -1,6 +1,7 @@
 import config
 
 from services.browser import BotMaker
+from services.database.manager import DBManager
 from services.facebook.group import FBGroup
 from services.google.token import TokenRetriever
 from services.google.youtube import YouTubeCtl
@@ -14,6 +15,7 @@ class ServiceManager:
         gtoken = token_retr.retrieve_google_api_token(
             config.google_api_token_path, config.google_api_scope
         )
+        self.__db_manager = DBManager(config.db_path)
         self.__youtube = YouTubeCtl(gtoken, config.google_api_scope)
         self.__youtube.start_service()
         self.__browser = BotMaker(browser="Chrome", remote=True, port=8989)
@@ -35,12 +37,22 @@ class ServiceManager:
             video_id = link.split(r"=")[1]
         return video_id
 
+    def new_link_posted(self):
+        self.__facebook.goto_announc()
+        theme_day, theme1, theme2 = self.__facebook.get_todays_theme()
+        link = self.__facebook.get_latest_video_link() 
+        exists = self.__db_manager.link_exists(theme_day, theme1, theme2, link)
+        if exists:
+            return (False, ())
+        else:
+            return (True, (theme_day, theme1, theme2, link))
+
     def add_to_playlist(self, playlist_id:str, link:str):
         video_id = self.__get_video_id(link)
         self.__youtube.add_video_to_playlist(playlist_id, video_id)
         if self.log: print(f"[+] ADDED {link} to the playlist {playlist_id}")
 
-    def search_in_playlist(self, title) -> bool:
+    def playlist_exists(self, title) -> bool:
         playlists = self.__get_playlists()
         return (title in playlists)
 
